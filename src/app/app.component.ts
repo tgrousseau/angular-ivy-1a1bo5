d6c41aedae04a7aa930dea7b7cbff7ed3a9fc11b
tgrousseau <thomas.grousseau@miam.tech>
2022-05-03 14:58:09 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component, VERSION } from '@angular/core';
 
+export interface Recipe {
+  title: string;
+  picture: string;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  recipe: Recipe;
+
+  constructor() {
+    this.recipe = {
+      title: 'Quiche Lorraine',
+      picture:
+        'https://assets.afcdn.com/recipe/20161128/28118_w1024h1024c1cx845cy3505.jpg',
+    };
+  }
+}
